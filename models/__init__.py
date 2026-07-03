578ee443667645871ddff3880a8fa554cbfd8035
ElijahLimJC <elijahlim57@gmail.com>
2026-07-03 15:05:56 +0800

    feat: add project-wide APP_TIMEZONE constant (UTC+8)

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+"""RoadBlock models package."""
+
+from datetime import timezone, timedelta
+
+# Project-wide timezone: UTC+8 (Singapore/Malaysia/Philippines)
+APP_TIMEZONE = timezone(timedelta(hours=8))
